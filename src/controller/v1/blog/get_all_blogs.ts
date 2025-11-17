a296a7754fa9c3da893128eda64c700ab9cd27cd
zalak-chauhan <chauhanzalak123@gmail.com>
2025-11-17 23:54:41 +0100

    Get All Blog functionality

---
+++ src/controller/v1/blog/get_all_blogs.ts
@@ -0,0 +1,69 @@
+// Custom modules
+import config from '@/config';
+import { logger } from '@/lib/winston';
+
+// Modules
+import Blog from '@/model/blog';
+import User from '@/model/user';
+
+// Types
+import type { Request, Response } from 'express';
+
+interface QueryType {
+    status?: 'draft' | 'published'
+};
+
+const getAllBlogs = async(req: Request, res:Response) => {
+    try{
+
+        // Get users's objects id
+        const userId = req.userId;
+        console.log('user id - ', userId);
+        // Set limit & offset for req.query
+        const limit = parseInt(req.query.limit as string) || config.defaultResponseLimit;
+        const offset = parseInt(req.query.offset as string) || config.defaultResponseOffset;
+        // Find total blogs fron db
+        const total = await Blog.countDocuments();
+
+        // find role based on the object id in user db
+        const user = await User.findById(userId).select('role').lean().exec();
+        console.log('user Role - ', user)
+        const query : QueryType = {};
+
+        // Show only published post to normal users
+        if(user?.role === 'user'){
+            query.status = 'published';
+        };
+
+        
+        const blogs = await Blog.find(query)
+            .select('-banner.publicId -__v')
+            .populate('author', '-createdAt -updatedAt -__v')
+            .limit(limit)
+            .skip(offset)
+            .sort({ createdAt: -1 })
+            .lean()
+            .exec();
+
+        res.status(200).json({
+            limit,
+            offset,
+            total,
+            blogs
+        });
+
+        logger.info('Blogs fetching Successfull');
+
+
+    } catch (err) {
+        res.status(500).json({
+            code : 'ServerError',
+            message : 'Internal Server Error',
+            error : err
+        });
+
+        logger.error('Error while fetching Blogs', err);
+    } 
+};
+
+export default getAllBlogs;
--- src/routes/v1/blog.ts
+++ src/routes/v1/blog.ts
@@ -1,6 +1,7 @@
 // Node modules
 import { Router } from 'express';
 import multer from 'multer';
+import { query } from 'express-validator';
 
 // Middlewares or validations
 import authenticate from '@/middlewares/auth/authenticate';
@@ -8,11 +9,14 @@ import authorize from'@/middlewares/user/authorize';
 import validationError from '@/middlewares/auth/validationError';
 import uploadBlogBanner from '@/middlewares/blog/uploadBannerImage';
 import {CreateBlogValidation} from '@/middlewares/blog/blogCreateValidation';
+import {GetBlogValidation} from '@/middlewares/blog/getBlogsValidation';
 
 
 // Controllers
 // create
 import createBlog from '@/controller/v1/blog/create_blog';
+import getAllBlogs from '@/controller/v1/blog/get_all_blogs';
+
 
 // Multer upload
 const upload = multer();
@@ -29,6 +33,15 @@ router.post(
     CreateBlogValidation,
     validationError,
     createBlog);
+
+    router.get(
+        '/',
+        authenticate,
+        authorize(['admin','user']),
+        GetBlogValidation,
+        validationError,
+        getAllBlogs
+    )
 
 
 export default router;
